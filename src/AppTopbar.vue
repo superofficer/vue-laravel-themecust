4310f7b16dbdb13b391bbc8f8f7b0eb76c887d9e
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2020-02-27 12:02:17 +0000

    topbar transition added

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -16,38 +16,41 @@
 				<i class="material-icons">menu</i>
 			</button>
 			<ul :class="topbarItemsClass">
-				<li v-if="profileMode === 'top' || horizontal" :class="['profile-item', {'active-top-menu': activeTopbarItem === 'profile'}]">
+				<li v-if="profileMode === 'top' || horizontal"
+					:class="['profile-item', {'active-top-menu': activeTopbarItem === 'profile'}]">
 					<button class="p-link" @click="onTopbarItemClick($event,'profile')">
 						<img class="profile-image" src="assets/layout/images/avatar.png" alt="Profile"/>
 						<span class="topbar-item-name">Jane Williams</span>
 					</button>
 
-					<ul class="ultima-menu animated fadeInDown">
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">person</i>
-								<span>Profile</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">security</i>
-								<span>Privacy</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">settings_applications</i>
-								<span>Settings</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">power_settings_new</i>
-								<span>Logout</span>
-							</button>
-						</li>
-					</ul>
+					<transition name="layout-submenu-container">
+						<ul class="ultima-menu animated fadeInDown" v-show="activeTopbarItem === 'profile'">
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">person</i>
+									<span>Profile</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">security</i>
+									<span>Privacy</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">settings_applications</i>
+									<span>Settings</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">power_settings_new</i>
+									<span>Logout</span>
+								</button>
+							</li>
+						</ul>
+					</transition>
 				</li>
 
 				<li :class="[{'active-top-menu': activeTopbarItem === 'settings'}]">
@@ -56,32 +59,34 @@
 						<span class="topbar-item-name">Settings</span>
 					</button>
 
-					<ul class="ultima-menu animated fadeInDown">
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">palette</i>
-								<span>Change Theme</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">favorite_border</i>
-								<span>Favorites</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">lock</i>
-								<span>Lock Screen</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">wallpaper</i>
-								<span>Wallpaper</span>
-							</button>
-						</li>
-					</ul>
+					<transition name="layout-submenu-container">
+						<ul class="ultima-menu animated fadeInDown" v-show="activeTopbarItem === 'settings'">
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">palette</i>
+									<span>Change Theme</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">favorite_border</i>
+									<span>Favorites</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">lock</i>
+									<span>Lock Screen</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">wallpaper</i>
+									<span>Wallpaper</span>
+								</button>
+							</li>
+						</ul>
+					</transition>
 				</li>
 
 				<li :class="[{'active-top-menu': activeTopbarItem === 'messages'}]">
@@ -91,38 +96,40 @@
 						<span class="topbar-item-name">Messages</span>
 					</button>
 
-					<ul class="ultima-menu animated fadeInDown">
-						<li role="menuitem">
-							<button  class="p-link topbar-message">
-								<img src="assets/layout/images/avatar1.png" width="35" alt="avatar1" />
-								<span>Give me a call</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button  class="p-link topbar-message">
-								<img src="assets/layout/images/avatar2.png" width="35" alt="avatar2"/>
-								<span>Sales reports attached</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button  class="p-link topbar-message">
-								<img src="assets/layout/images/avatar3.png" width="35" alt="avatar3"/>
-								<span>About your invoice</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button  class="p-link topbar-message">
-								<img src="assets/layout/images/avatar2.png" width="35" alt="avatar2"/>
-								<span>Meeting today at 10pm</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button  class="p-link topbar-message">
-								<img src="assets/layout/images/avatar4.png" width="35" alt="avatar4"/>
-								<span>Out of office</span>
-							</button>
-						</li>
-					</ul>
+					<transition name="layout-submenu-container">
+						<ul class="ultima-menu animated fadeInDown" v-show="activeTopbarItem === 'messages'">
+							<li role="menuitem">
+								<button class="p-link topbar-message">
+									<img src="assets/layout/images/avatar1.png" width="35" alt="avatar1"/>
+									<span>Give me a call</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link topbar-message">
+									<img src="assets/layout/images/avatar2.png" width="35" alt="avatar2"/>
+									<span>Sales reports attached</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link topbar-message">
+									<img src="assets/layout/images/avatar3.png" width="35" alt="avatar3"/>
+									<span>About your invoice</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link topbar-message">
+									<img src="assets/layout/images/avatar2.png" width="35" alt="avatar2"/>
+									<span>Meeting today at 10pm</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link topbar-message">
+									<img src="assets/layout/images/avatar4.png" width="35" alt="avatar4"/>
+									<span>Out of office</span>
+								</button>
+							</li>
+						</ul>
+					</transition>
 				</li>
 
 				<li :class="[{'active-top-menu': activeTopbarItem === 'notifications'}]">
@@ -132,35 +139,38 @@
 						<span class="topbar-item-name">Notifications</span>
 					</button>
 
-					<ul class="ultima-menu animated fadeInDown">
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">bug_report</i>
-								<span>Pending tasks</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">event</i>
-								<span>Meeting today at 3pm</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">file_download</i>
-								<span>Download documents</span>
-							</button>
-						</li>
-						<li role="menuitem">
-							<button class="p-link" >
-								<i class="material-icons">flight</i>
-								<span>Book flight</span>
-							</button>
-						</li>
-					</ul>
+					<transition name="layout-submenu-container">
+						<ul class="ultima-menu animated fadeInDown" v-show="activeTopbarItem === 'notifications'">
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">bug_report</i>
+									<span>Pending tasks</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">event</i>
+									<span>Meeting today at 3pm</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">file_download</i>
+									<span>Download documents</span>
+								</button>
+							</li>
+							<li role="menuitem">
+								<button class="p-link">
+									<i class="material-icons">flight</i>
+									<span>Book flight</span>
+								</button>
+							</li>
+						</ul>
+					</transition>
 				</li>
 
-				<li :class="['search-item', {'active-top-menu': activeTopbarItem === 'search'}]" @click="onTopbarItemClick($event,'search')">
+				<li :class="['search-item', {'active-top-menu': activeTopbarItem === 'search'}]"
+					@click="onTopbarItemClick($event,'search')">
 					<span class="md-inputfield">
 						<InputText type="text" v-model="searchText"/>
 						<label>Search</label>
@@ -173,47 +183,47 @@
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			searchText: ''
-		}
-	},
-	props: {
-		profileMode: String,
-		horizontal: {
-			type: Boolean,
-			default: false
+	export default {
+		data() {
+			return {
+				searchText: ''
+			}
 		},
-		topbarMenuActive: {
-			type: Boolean,
-			default: false
+		props: {
+			profileMode: String,
+			horizontal: {
+				type: Boolean,
+				default: false
+			},
+			topbarMenuActive: {
+				type: Boolean,
+				default: false
+			},
+			activeTopbarItem: String,
 		},
-		activeTopbarItem: String,
-	},
-    methods: {
-        onMenuButtonClick(event) {
-            this.$emit('menubutton-click', event);
-        },
-		onTopbarMenuButtonClick(event) {
-			this.$emit('topbar-menubutton-click', event)
+		methods: {
+			onMenuButtonClick(event) {
+				this.$emit('menubutton-click', event);
+			},
+			onTopbarMenuButtonClick(event) {
+				this.$emit('topbar-menubutton-click', event)
+			},
+			onTopbarItemClick(event, item) {
+				this.$emit('topbaritem-click', {originalEvent: event, item: item})
+			},
+			onRightPanelButtonClick(event) {
+				this.$emit('rightpanel-button-click', event)
+			},
+			goDashboard() {
+				window.location = "/#/"
+			}
 		},
-		onTopbarItemClick(event, item) {
-			this.$emit('topbaritem-click', {originalEvent:event, item:item})
-		},
-		onRightPanelButtonClick(event) {
-			this.$emit('rightpanel-button-click', event)
-		},
-		goDashboard(){
-			window.location = "/#/"
-		}
-    },
-	computed: {
-		topbarItemsClass() {
-			return ['topbar-items animated fadeInDown', {
-				'topbar-items-visible': this.topbarMenuActive
-			}];
+		computed: {
+			topbarItemsClass() {
+				return ['topbar-items animated fadeInDown', {
+					'topbar-items-visible': this.topbarMenuActive
+				}];
+			}
 		}
 	}
-}
 </script>
