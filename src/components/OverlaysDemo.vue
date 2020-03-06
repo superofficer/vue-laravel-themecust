6dfabba2c2fe9953dfbe8f2650a71b59480ce13b
merve7 <merve7@fd9223c1-c77d-41e1-af93-17165896985c>
2020-03-06 08:36:53 +0000

    overlay fix

--- src/components/OverlaysDemo.vue
+++ src/components/OverlaysDemo.vue
@@ -28,12 +28,11 @@
 			<div class="card">
 				<h1>Dialog</h1>
 				<Dialog header="Godfather I" :visible.sync="display" :style="{width: '30vw'}" :modal="true">
-					<p>The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding.
-						His beloved son Michael has just come home from the war, but does not intend to become part of his father's business.
-						Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family,
-						kind and benevolent to those who give respect,
-						but given to ruthless violence whenever anything stands against the good of the family.
-					</p>
+					The story begins as Don Vito Corleone, the head of a New York Mafia family, oversees his daughter's wedding.
+					His beloved son Michael has just come home from the war, but does not intend to become part of his father's business.
+					Through Michael's life the nature of the family business becomes clear. The business of the family is just like the head of the family,
+					kind and benevolent to those who give respect,
+					but given to ruthless violence whenever anything stands against the good of the family.
 					<template #footer>
 						<Button label="Yes" @click="close" icon="pi-md-check"/>
 						<Button label="No" @click="close" icon="pi-md-close" class="p-button-secondary"/>
