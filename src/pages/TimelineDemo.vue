ad8d12eafaa7a615e4b0f430c0479dc33ec9696d
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-02-18 17:41:31 +0300

    fix style

--- src/pages/TimelineDemo.vue
+++ src/pages/TimelineDemo.vue
@@ -105,24 +105,22 @@ export default {
 ::v-deep(.p-timeline-event-opposite) {
     line-height: 1;
 }
-
 @media screen and (max-width: 960px) {
-    ::v-deep(.customized-timeline .p-timeline-event:nth-child(even)) {
-        flex-direction: row !important;
-        
-        .p-timeline-event-content {
-            text-align: left !important;
+    ::v-deep(.customized-timeline) {
+        .p-timeline-event:nth-child(even) {
+            flex-direction: row !important;
+            
+            .p-timeline-event-content {
+                text-align: left !important;
+            }
+        }
+        .p-timeline-event-opposite {
+            flex: 0;
+        }
+        .p-card {
+            margin-top: 1rem;
         }
     }
-
-    ::v-deep(.customized-timeline .p-timeline-event-opposite) {
-        flex: 0;
-    }
-
-    ::v-deep(.customized-timeline .p-card) {
-        margin-top: 1rem;
-    }
-    
 }
 
 </style>
