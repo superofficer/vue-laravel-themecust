343b7b0efa78555b47ecaa1cfc66e0acd15094fb
Tuğçe Küçükoğlu <tugcekucukoglu.860@gmail.com>
2021-02-18 15:10:05 +0300

    update page

--- src/components/EmptyPage.vue
+++ src/components/EmptyPage.vue
@@ -3,7 +3,7 @@
 		<div class="p-col-12">
 			<div class="card">
 				<h5>Empty Page</h5>
-				<p>Use this page to start from scratch and place your custom content.</p>
+				<p>This is your empty page template to start building beautiful applications.</p>
 			</div>
 		</div>
 	</div>
